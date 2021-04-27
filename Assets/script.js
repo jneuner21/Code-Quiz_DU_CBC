9d577ccef75553c6346ea42859c1f237a5ae014d
jneuner21 <72330999+jneuner21@users.noreply.github.com>
2021-04-27 00:29:02 -0600

    updated all html articles to have 'hide' class so that when correct answer is slected it moves to next question. updated style so all buttons, fonts, etc matched aesthetic. wrote all js code to make cuttons clickable and move from question to questiong

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,48 @@
-function setTime() {
-    // Sets interval in variable
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
-  
-      if(secondsLeft === 0) {
-        // Stops execution of action at set interval
-        clearInterval(timerInterval);
-        // Calls function to create and append image
-        sendMessage();
-      }
-  
-    }, 1000);
-  }
+var startBtn= document.querySelector("#startButton");
+var mainSection= document.querySelector(".mainContainer")
+var quizContainer= document.querySelector(".quizContainer")
+var quizSet1= document.querySelector(".quizSet1")
+var quizSet2= document.querySelector(".quizSet2")
+var quizSet3= document.querySelector(".quizSet3")
+var gameOverSection= document.querySelector(".gameOverSection")
+
+
+function startGame() {
+  quizContainer.classList.add("hide");
+  quizSet1.classList.remove("hide");
+}
+
+startBtn.addEventListener("click", startGame);
+
+var correctAnswer= document.getElementById("correctAnswer")
+
+
+function nextQuestion() {
+  quizSet1.classList.add("hide");
+  quizSet2.classList.remove("hide");
+}
+
+correctAnswer.addEventListener("click",nextQuestion)
+var correctAnswer2= document.getElementById("correctAnswer2")
+
+
+function nextQuestion2() {
+  quizSet2.classList.add("hide");
+  quizSet3.classList.remove("hide");
+}
+
+correctAnswer2.addEventListener("click",nextQuestion2)
+var correctAnswer3= document.getElementById("correctAnswer3")
+
+
+
+function nextQuestion3() {
+  quizSet3.classList.add("hide");
+  gameOverSection.classList.remove("hide");
+}
+
+gameOverSection.addEventListener("click", gameOverSection)
+
+
+//below is an example of a click listen
+//document.getElementById("myBtn").addEventListener("click", function() {
